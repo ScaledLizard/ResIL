07c382d0f21b428ab44a1afa6124f55f7f2a52ba
Bjoern Ganster <usibganste@usu.grp>
2016-03-17 12:45:32 +0100

    Updated input libs for 2016

---
+++ projects/msvc2015/clean.bat
@@ -0,0 +1,82 @@
+del /s /q debug
+del /s /q x64
+del /s /q Release-libcmt
+del /s /q Release-msvcrt
+del *.sdf
+
+del /s /q ILMbase\debug
+del /s /q ILMbase\x64
+del /s /q ILMbase\Release-libcmt
+del /s /q ILMbase\Release-msvcrt
+
+del /s /q IL\debug
+del /s /q IL\x64
+del /s /q IL\Release-libcmt
+del /s /q IL\Release-msvcrt
+
+del /s /q ILU\debug
+del /s /q ILU\x64
+del /s /q ILU\Release-libcmt
+del /s /q ILU\Release-msvcrt
+
+del /s /q ILUT\debug
+del /s /q ILUT\x64
+del /s /q ILUT\Release-libcmt
+del /s /q ILUT\Release-msvcrt
+
+del /s /q jpeg\debug
+del /s /q jpeg\x64
+del /s /q jpeg\Release-libcmt
+del /s /q jpeg\Release-msvcrt
+
+del /s /q lcms\debug
+del /s /q lcms\x64
+del /s /q lcms\Release-libcmt
+del /s /q lcms\Release-msvcrt
+
+del /s /q libjasper\debug
+del /s /q libjasper\x64
+del /s /q libjasper\Release-libcmt
+del /s /q libjasper\Release-msvcrt
+
+del /s /q libmng\debug
+del /s /q libmng\x64
+del /s /q libmng\Release-libcmt
+del /s /q libmng\Release-msvcrt
+
+del /s /q libpng\debug
+del /s /q libpng\x64
+del /s /q libpng\Release-libcmt
+del /s /q libpng\Release-msvcrt
+
+del /s /q OpenEXR\debug
+del /s /q OpenEXR\x64
+del /s /q OpenEXR\Release-libcmt
+del /s /q OpenEXR\Release-msvcrt
+
+del /s /q test\debug
+del /s /q test\x64
+del /s /q test\Release-libcmt
+del /s /q test\Release-msvcrt
+
+del /s /q testAPI2\debug
+del /s /q testAPI2\x64
+del /s /q testAPI2\Release-libcmt
+del /s /q testAPI2\Release-msvcrt
+
+del /s /q testAPI2MT\debug
+del /s /q testAPI2MT\x64
+del /s /q testAPI2MT\Release-libcmt
+del /s /q testAPI2MT\Release-msvcrt
+
+del /s /q TIFF\debug
+del /s /q TIFF\x64
+del /s /q TIFF\Release-libcmt
+del /s /q TIFF\Release-msvcrt
+
+del /s /q zlib\debug
+del /s /q zlib\x64
+del /s /q zlib\Release-libcmt
+del /s /q zlib\Release-msvcrt
+
+pause
